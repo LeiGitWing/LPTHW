d0e4eb9735f129dd89be435b14a0ff9934606f8e
claraligz@gmail.com <jlii0288@student.monash.edu>
2023-07-13 16:31:46 +0800

    Add solution 1 to 5

---
+++ lpthw/ex3-3.py
@@ -0,0 +1,13 @@
+# This is a program to calculate BMI
+# BMI = weight / (height^2)
+
+print("What's your weight in kg?")
+print(55)
+
+print("What's your height in cm?")
+print(1.53)
+
+print("Calculating...")
+
+print("Your BMI is:")
+print(55 / (1.53 * 1.53))
---
+++ lpthw/ex3.py
@@ -0,0 +1,37 @@
+# print statement
+print("I will now count my chickens:")
+
+# calculate the number of hens
+print("Hens", 25 + 30 / 6)
+# calculate the number of roosters
+print("Roosters", 100 - 25 * 3 % 4)
+
+# print statement
+print("Now I will count the eggs:")
+
+# calculating
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# print statement
+print("Is it true that 3 + 2 < 5 -7?")
+
+# print if is ture or false that 3 + 2 < 5 - 7
+print(3 + 2 < 5 - 7)
+
+# print the resule of 3 + 2
+print("What is 3 + 2?", 3 + 2)
+# print the resule of 5 - 7
+print("What is 5 - 7?", 5 - 7)
+
+# print statement
+print("Oh, that's why it's False.")
+
+# print statement
+print("How about some more.")
+
+# determine if is it true or false that 5 > -2
+print("Is it greater?", 5 > -2)
+# determine if is it true or false that 5 >= -2
+print("Is it grater or equal?", 5 >= -2)
+# determine if is it true or false that 5 <= -2
+print("Is it less or equal?", 5 <= -2)
